7cff1009008c556da8b0cc4f60a8632d2c183d40
ellendeveth <ellen.deveth@hotmail.com>
2021-12-23 16:31:11 +0100

    search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View,Text, Button, TextInput } from 'react-native';
 import WeatherInfo from '../components/WeatherInfo';
+import Searchbar from '../components/SearchBar';
 
 
 
 
 const HomeScreen = ({ navigation }) => {
   
-  const [currentWeather, setCurrentWeather] = useState ({});
-  let city = 'Mechelen'
-
-  const getWeather = async (city) => {
-    try {
-      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=8351c874df924b50b37193648212212&q=${city}`)
-      const result = await response.json()
-      
-      setCurrentWeather(result.current);
-      console.log(currentWeather);
-    } catch (error) {
-      console.error(error);
-    }
-    
-  }
+  
 
   useEffect(()=> {
     getWeather('Antwerp');
@@ -29,11 +16,11 @@ const HomeScreen = ({ navigation }) => {
   
   return (
       <View>
-          <TextInput placeholder="Vul een stad in" style={styles.input} onChangeText={getWeather}/>
-          
           <View>
+            <Searchbar getWeather = {getWeather}/> 
             <WeatherInfo currentWeather = {currentWeather} />
           </View>
+
           <Button title="Weervoorspelling 30 dagen" onPress={() => navigation.navigate('Details')}/>
       </View>
   )
